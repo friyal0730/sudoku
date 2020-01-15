12726b3499d9456d9fe4cd9f35c1129b2f998200
friyal0730 <1428606198@qq.com>
2020-01-16 00:13:58 +0800

    solve.h

---
+++ sudoku/solve.h
@@ -0,0 +1,67 @@
+#pragma once
+#ifndef _CHECK_H_
+#define _CHECK_H_
+
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+#define MAX 1000005
+using namespace std;
+
+bool compare(char* a, const char* b);
+
+class Check {
+public:
+	int argc;
+	char** argv;
+	int flag;
+	Check(int argc, char** argv)
+	{
+		this->argc = argc;
+		this->argv = argv;
+		checkinput();
+	}
+
+	int checkinput()
+	{
+		if (argc != 3)   //输入格式不正确
+		{
+			cout << "Illegal paramater number" << endl;
+			cout << "Input like this: [sudoku.exe -c n] or [sudoku.exe -s path]" << endl;
+			return 1;
+		}
+
+		if (!(compare(argv[1], "-c") || compare(argv[1], "-s")))   //字母错误
+		{
+			cout << "The first parameter should only be -c or -s" << endl;
+			cout << "-c means to generate the sudoku to file." << endl;
+			cout << "-s means to solve the sudoku from the file." << endl;
+			return 2;
+		}
+
+		if (compare(argv[1], "-c"))   //创造数独终盘
+		{
+			
+		}
+
+		if (compare(argv[1], "-s"))   //解题
+		{
+			
+		}
+		else 
+		{
+
+		}
+			
+	}
+};
+
+bool compare(char* a, const char* b)  //用于判断字符串的函数
+{
+	
+}
+#endif
+#pragma once
