59caa22e53486a43afd4de5b1467d77abcc87984
friyal0730 <1428606198@qq.com>
2020-01-16 22:09:54 +0800

    项目完成

--- sudoku/main.cpp
+++ sudoku/main.cpp
@@ -2,11 +2,11 @@
 #include <string>
 #include <cstring>
 #include <cstdio>
-
+#include "solve.h"
 using namespace std;
 
 int main(int argc, char** argv)
 {
-
+	solve sol(argc, argv);
 	return 0;
 }
