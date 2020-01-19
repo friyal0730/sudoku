692b985152e389aae7e32204b04c5b0653bad2c2
friyal0730 <59201660+friyal0730@users.noreply.github.com>
2020-01-19 22:12:13 +0800

    Add files via upload

---
+++ sudoku/base.h
@@ -0,0 +1,282 @@
+#pragma once
+#ifndef __base_H__
+#define __base_H__
+
+#include <cstdio>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+static int Sudoku[9][9] = {//基础数独终盘
+	{ 2, 9, 3, 4, 5, 1, 7, 8, 6 },
+	{ 5, 4, 1, 7, 8, 6, 9, 2, 3 },
+	{ 8, 7, 6, 9, 2, 3, 4, 5, 1 },
+	{ 9, 2, 4, 3, 1, 5, 8, 6, 7 },
+	{ 1, 3, 5, 8, 6, 7, 2, 9, 4 },
+	{ 6, 8, 7, 2, 9, 4, 3, 1, 5 },
+	{ 3, 5, 9, 1, 4, 2, 6, 7, 8 },
+	{ 4, 1, 2, 6, 7, 8, 5, 3, 9 },
+	{ 7, 6, 8, 5, 3, 9, 1, 4, 2 } };
+
+static int Sudoku_backup[9][9] = {//基础数独终盘
+	{ 2, 9, 3, 4, 5, 1, 7, 8, 6 },
+	{ 5, 4, 1, 7, 8, 6, 9, 2, 3 },
+	{ 8, 7, 6, 9, 2, 3, 4, 5, 1 },
+	{ 9, 2, 4, 3, 1, 5, 8, 6, 7 },
+	{ 1, 3, 5, 8, 6, 7, 2, 9, 4 },
+	{ 6, 8, 7, 2, 9, 4, 3, 1, 5 },
+	{ 3, 5, 9, 1, 4, 2, 6, 7, 8 },
+	{ 4, 1, 2, 6, 7, 8, 5, 3, 9 },
+	{ 7, 6, 8, 5, 3, 9, 1, 4, 2 } };
+
+class Base {
+public:
+	double totTime;
+	int count;
+	struct node
+	{
+		int num;
+		bool dir;
+	};
+
+	struct node location[9] = { {0,0} };
+	FILE* file_write;
+
+	Base(int number, FILE* file)
+	{
+		this->count = number;
+		file_write = file;
+		totTime = 0;
+		for (int i = 0; i < 9; i++)
+		{
+			location[i].num = i;
+			location[i].dir = true;
+		}
+	}
+
+	void generate()   //生成函数
+	{
+		int number = 0;
+		while (number < count)
+		{
+			Out();
+			number++;
+
+			Line_exchange_floor(&number);//换上面部分的行
+			Line_exchange_middle(&number);//换中间部分的行
+			Line_exchange_ground(&number);//换下面部分的行
+
+			if (number < count)
+			{
+				TransForm();
+				Change();
+			}
+		}
+	}
+
+
+private:
+	void Out()   //输出函数
+	{
+		int pointer = 0;
+		char temp[165];
+		memset(temp, 0, sizeof(temp));
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				temp[pointer] = Sudoku[i][j] + '0';
+				pointer++;
+				if (j != 8)
+				{
+					temp[pointer] = ' ';
+					pointer++;
+				}
+			}
+			temp[pointer] = '\n';
+			pointer++;
+		}
+
+		temp[pointer] = '\n';
+		fputs(temp, file_write);
+	}
+	   
+	void TransForm()
+	{
+		int move = 0;
+		int move_num = 0;
+		for (int i = 1; i <= 8; i++)
+		{
+			if (location[i].dir && (i > 1))
+			{
+				bool moveable;
+				moveable = location[i].num > location[i - 1].num;
+				if (moveable)
+				{
+					move = move_num > location[i].num ? move : i;
+					move_num = move_num > location[i].num ? move_num : location[i].num;
+				}
+			}
+			else if (!location[i].dir && (i < 8))
+			{
+				bool moveable;
+				moveable = location[i].num > location[i + 1].num;
+				if (moveable)
+				{
+					move = move_num > location[i].num ? move : i;
+					move_num = move_num > location[i].num ? move_num : location[i].num;
+				}
+			}
+		}
+
+		int temp = move_num;
+		bool temp_dir = location[move].dir;
+		if (temp_dir)
+		{
+			location[move].num = location[move - 1].num;
+			location[move].dir = location[move - 1].dir;
+			location[move - 1].num = temp;
+			location[move - 1].dir = temp_dir;
+		}
+		else
+		{
+			location[move].num = location[move + 1].num;
+			location[move].dir = location[move + 1].dir;
+			location[move + 1].num = temp;
+			location[move + 1].dir = temp_dir;
+		}
+
+
+		for (int i = 1; i <= 8; i++)
+		{
+			if (location[i].num > move_num)
+			{
+				location[i].dir = !location[i].dir;
+			}
+		}
+	}
+
+	void Change()
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				if (Sudoku_backup[i][j] < 9)
+				{
+					Sudoku[i][j] = location[Sudoku_backup[i][j]].num;
+				}
+			}
+		}
+	}
+
+	void Line_exchange_floor(int* number)
+	{
+		if (*number < count)
+		{
+			swap_line(2, 1);
+			*number = (*number) + 1;
+			Out();
+			Line_exchange_middle(number);
+		}
+		if (*number < count)
+		{
+			swap_line(2, 1);
+		}
+	}
+
+	void Line_exchange_middle(int* number)
+	{
+		if (*number < count)
+		{
+			swap_line(3, 4);
+			*number = (*number) + 1;
+			Out();
+			Line_exchange_ground(number);
+		}
+		if (*number < count)
+		{
+			swap_line(4, 5);
+			*number = (*number) + 1;
+			Out();
+			Line_exchange_ground(number);
+		}
+		if (*number < count) {
+			swap_line(3, 5);
+			*number = (*number) + 1;
+			Out();
+			Line_exchange_ground(number);
+		}
+
+		if (*number < count) {
+			swap_line(3, 4);
+			*number = (*number) + 1;
+			Out();
+			Line_exchange_ground(number);
+		}
+
+		if (*number < count)
+		{
+			swap_line(4, 5);
+			*number = (*number) + 1;
+			Out();
+			Line_exchange_ground(number);
+		}
+		if (*number < count)
+		{
+			swap_line(3, 5);
+		}
+	}
+
+	void Line_exchange_ground(int* number)
+	{
+		if (*number < count)
+		{
+			swap_line(6, 7);
+			Out();
+			*number = (*number) + 1;
+		}
+		if (*number < count)
+		{
+			swap_line(7, 8);
+			Out();
+			*number = (*number) + 1;
+		}
+		if (*number < count)
+		{
+			swap_line(6, 8);
+			Out();
+			*number = (*number) + 1;
+		}
+		if (*number < count)
+		{
+			swap_line(6, 7);
+			Out();
+			*number = (*number) + 1;
+		}
+		if (*number < count)
+		{
+			swap_line(7, 8);
+			Out();
+			*number = (*number) + 1;
+		}
+		if (*number < count)
+		{
+			swap_line(6, 8);
+		}
+	}
+
+	void swap_line(int a, int b)
+	{
+		int temp;
+		for (int i = 0; i < 9; i++)
+		{
+			temp = Sudoku[a][i];
+			Sudoku[a][i] = Sudoku[b][i];
+			Sudoku[b][i] = temp;
+		}
+	}
+};
+
+#endif
---
+++ sudoku/solve.h
@@ -0,0 +1,110 @@
+/* 
+ * solve.h
+ * 类名：solve
+ * 作用：检查判断输入参数，并创建相应的数独生成对象或者数独解决对象
+ * 
+ */
+#pragma once
+#ifndef _solve_H_
+#define _solve_H_
+
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <iostream>
+#include <cstring>
+#include"base.h"
+#include"answer.h"
+#define MAX 1000005
+using namespace std;
+
+class solve {
+public:
+	int argc;
+	char** argv;
+	int flag = 0;
+	solve(int argc, char** argv)
+	{
+		this->argc = argc;
+		this->argv = argv;
+		solveinput();   
+	}
+
+	int solveinput() //判断并执行命令
+	{
+		if (argc != 3) //输入格式不正确
+		{
+			cout << "Illegal paramater number" << endl;
+			cout << "Input like this: [sudoku.exe -c n] or [sudoku.exe -s path]" << endl;
+			return 1;
+		}
+
+		if (strcmp(argv[1], "-c") && strcmp(argv[1], "-s"))   //字母错误
+		{
+			cout << "The first parameter should only be -c or -s" << endl;
+			cout << "-c means to generate the sudoku to file." << endl;
+			cout << "-s means to solve the sudoku from the file." << endl;
+			return 2;
+		}
+
+		if (!strcmp(argv[1], "-c"))   //创造数独终盘
+		{
+			int sum = 0;   //sudoku的个数
+			unsigned long long len = strlen(argv[2]);
+			for (int i = 0; i < len; i++)
+			{
+				if (!(argv[2][i] >= '0' && argv[2][i] <= '9'))   //输入的字符不合法（不是数字）
+				{
+					cout << "The third paramater after -c should be number that indicate the sudoku you want." << endl;
+					if (argv[2][i] == '+' || argv[2][i] == '-' || argv[2][i] == '/' || argv[2][i] == '*')
+					{
+						cout << "Please input the number!" << endl;
+						return 8;
+					}
+					return 3;
+				}
+				sum = 10 * sum + argv[2][i] - '0';
+			}
+
+			if (sum > MAX || sum < 1)   //数字过大
+			{
+				cout << "The number is too large,the number should be 1-1000000" << endl;
+				return 4;
+			}
+			/*----------------------------------*/
+			/*创建数独终盘对象*/
+			/*...........*/
+
+			FILE* file;
+			file = freopen("sudoku.txt", "w", stdout);   //没有文件时可以创造
+			Base base(sum, file);    //调用Base
+			base.generate();
+			/*----------------------------------*/
+
+			return 5;
+		}
+
+		if (!strcmp(argv[1], "-s"))   //解题
+		{
+			FILE* ans;
+			FILE* question;   //数独题目
+			question = freopen(argv[2], "r", stdin);
+			if (!question)
+			{
+				cout << "The file path is not right,please check." << endl;
+				return 6;
+			}
+			/*----------------------------------*/
+			/*创建数独求解对象*/
+			/*...........*/
+			ans = freopen("sudoku.txt", "w", stdout);
+			Answer Answer(question, ans);   //调用Solver
+			flag = Answer.in();
+			/*----------------------------------*/
+			return 7;
+		}
+		
+		return 8;   //正常执行，消除警告
+	}
+};
+#endif
