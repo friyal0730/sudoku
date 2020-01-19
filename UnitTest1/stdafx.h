a1a5944c48d98a4b69478d727a46b3951ddea452
friyal0730 <59201660+friyal0730@users.noreply.github.com>
2020-01-19 22:10:54 +0800

    Add files via upload

---
+++ UnitTest1/stdafx.h
@@ -0,0 +1,36 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+
+
+// TODO: 在此处引用程序需要的其他头文件
+//#include "../sudoku/solve.h"
+//#include <cstdio>
+//#include <cstring>
+//#include <Windows.h>
+//#include <set>
+//#include <cassert>
+
+#ifndef STDAFX_H
+#define STDAFX_H
+
+#include "targetver.h"
+
+// 用于 CppUnitTest 的头文件
+#include "CppUnitTest.h"
+
+#include "../sudoku/solve.h"
+#include <cstdio>
+#include <cstring>
+#include <Windows.h>
+#include <set>
+#include <cassert>
+
+#endif //PCH_H
+
+
+
